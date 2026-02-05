d7d8776003a168ebd53861be15b3cb8eff3ee243
Jorge Gasco <97962858+JorGasco@users.noreply.github.com>
2026-02-05 05:16:48 +0000

    Function to update friend's interests.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -64,3 +64,14 @@ function findFriends(friends: Friend[], criterion: (f: Friend) => boolean): stri
 
 console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
 console.log(findFriends(friends, (friend) => friend.age < 35));
+
+function addInterest(friend: Friend, interest: string): string[] {
+    if (friend.interests === undefined) {
+        friend.interests = [];
+    }
+    friend.interests.push(interest);
+    return friend.interests;
+}
+
+console.log(addInterest(friends[0], 'Politics'))
+console.log(addInterest(friends[1], 'Politics'))
